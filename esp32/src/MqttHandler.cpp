302675543b033720b13ac6fa848c66a25c4e217d
FlorianLabarre <florian.labarre@epitech.eu>
2024-11-16 02:26:50 +0100

    fix(mqtt): Updated client ID

--- esp32/src/MqttHandler.cpp
+++ esp32/src/MqttHandler.cpp
@@ -4,31 +4,7 @@
 
 #include "MqttHandler.h"
 
-const char caCert[] PROGMEM = R"EOF(-----BEGIN CERTIFICATE-----
-MIID+TCCAuGgAwIBAgIUbHZXMwSEWWxvRvtlmdSbVE+y94AwDQYJKoZIhvcNAQEL
-BQAwgYsxCzAJBgNVBAYTAkZSMQ4wDAYDVQQIDAVQYXJpczEOMAwGA1UEBwwFUGFy
-aXMxDjAMBgNVBAoMBUFsaW5hMRMwEQYDVQQLDApBbGluYSBtcXR0MQ4wDAYDVQQD
-DAVBbGluYTEnMCUGCSqGSIb3DQEJARYYc2VydmljZUBtYXNzaXZldXNhZ2UuY29t
-MB4XDTI0MTExNTIzNTUyMVoXDTI5MTExNTIzNTUyMVowgYsxCzAJBgNVBAYTAkZS
-MQ4wDAYDVQQIDAVQYXJpczEOMAwGA1UEBwwFUGFyaXMxDjAMBgNVBAoMBUFsaW5h
-MRMwEQYDVQQLDApBbGluYSBtcXR0MQ4wDAYDVQQDDAVBbGluYTEnMCUGCSqGSIb3
-DQEJARYYc2VydmljZUBtYXNzaXZldXNhZ2UuY29tMIIBIjANBgkqhkiG9w0BAQEF
-AAOCAQ8AMIIBCgKCAQEAp3vWVOxj4aSfUY+gyCzU6jfVVRoyHkdcIJnmIXjxfNoW
-aaPCoIS+dWOGx4yCClKguZyWubUGC3MQ+u7cxPAd8pHNE0vqUruan7jNG/1cwayB
-drWQVwUPyCAXvRVZ0fy0Ej/JCe/IdurrVStstq6yOIXMAEIAN6kTHKrkbe8Vcgtg
-JtbqxNVAMgw2pRe5qsmg319IqlvmcTY9sA/9siCUV0Jo9ANYd0qmg3dDhrlE7OK7
-WwCXxxtvNpa66hUEIV8SzsKzcGmqQ3j4AeHk4E2u8mIYI8VS5VU3yq5LJB4d3BCt
-1ulbF4m6Ij40btXz3iIZdHqSY+GR/bNLVfnakS6SbQIDAQABo1MwUTAdBgNVHQ4E
-FgQU4oY0BqgoklY1a0Ut97lSefcRdCMwHwYDVR0jBBgwFoAU4oY0BqgoklY1a0Ut
-97lSefcRdCMwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAQhZu
-ToD1f51AYQ+aaI0nKPTvSfCEdkXdVGNuv6xzCLCcK9HfpwFS8Jn7PAMWxOY2R82M
-eeNfx7FKyuFs+K3o2+F96kJIiwzDEeS2+XwzlqAYFcjLZNkoOIWePexbw1RNWV2i
-XL+I8idQ3yWu87iQ1s36QSOUGNWKGIHp/RrXk5MAf3wyI4/dZ1Wa1JiQuJ9N9t50
-dHxIWheZlqIaLZg0kUgsZcuAHTICYNlPoQbrXa/hSnSYXToax+xM1Czc/CzCb0yB
-FJsvT50fgLOJHdJAaChnuI34sujqqfz/RbxAHFphVoSiHUKBuaoJf3zHT4d5Oxms
-nfDmhsuxhumdnYtuwA==
------END CERTIFICATE-----
-)EOF";
+const char caCert[] PROGMEM = R"EOF(REDACTED)EOF";
 
 MqttHandler::MqttHandler() : _client(_espClient) {}
 
@@ -102,8 +78,8 @@ void MqttHandler::send_client_info(const char *user, const char *pass, const cha
         reconnect();
     }
     snprintf(payload, sizeof(payload),
-             "{\"id\":\"%012llx\",\"user\":\"%s\",\"pass\":\"%s\",\"email\":\"%s\"}",
-             ESP.getEfuseMac(), user, pass, email);
+             "{\"id\":\"%s\",\"user\":\"%s\",\"pass\":\"%s\",\"email\":\"%s\"}",
+             clientId, user, pass, email);
 
     _client.publish("esp32/newClient", payload);
 }
